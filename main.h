8e9c8e57712e7ad1fc5c6384debfa25ec879eb6b
936lady <queenkleome@gmail.com>
2022-07-09 07:23:18 -0700

    tasks

--- main.h
+++ main.h
@@ -5,12 +5,8 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdarg.h>
-#include <limits.h>
 
-void _sprintf(const char *str);
 int _printf(const char *format, ...);
-void _count(int *ptr, char *str);
-char *_itoa(int num);
 
 
 #endif /* MAIN_H */
